f9a80c015569c220626c6b93864c156e907c4b47
bakhtiyaroveech <bakhtiyaroveech@gmail.com>
2024-06-07 16:12:34 +0500

    Initial commit

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import CardLIst from '@/components/CardLIst.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://ce1f5cf30cf45e0a.mokky.dev/favorites?_relations=items')
+
+    favorites.value = data.map((obj) => obj.item);
+
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+
+  <CardLIst :items="favorites" is-favorites />
+</template>
